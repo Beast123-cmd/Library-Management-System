c3f83dd7fefbc588c6a4110d32d8ebcb7108ecd5
Aayush Angal <aayushangal@Aayushs-MacBook-Air.local>
2026-05-19 21:58:34 +0530

    cleaned files

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Add the project root directory to the Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app import app
-
-# Expose the Flask app as 'app' for the Vercel Python runtime
